d0c9b8bad0a3e98198e9089284d099aa8306d11c
candle-lee <bri.candle.li@gmail.com>
2023-09-12 04:09:02 +0900

    fix: update routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from "react-router-dom";
 import App from './App.tsx'
 import './index.css'
 import 'flowbite';
@@ -8,8 +7,6 @@ import 'flowbite';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <Router>
-        <App />
-      </Router>
+    <App />
   </React.StrictMode>,
 )
