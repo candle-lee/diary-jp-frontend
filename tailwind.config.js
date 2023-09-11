b20edd4f077acef58dfe567992847fbceb1c21b0
candle-lee <bri.candle.li@gmail.com>
2023-09-11 20:38:54 +0900

    initial project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
