1b5bfc5987df509413b64c7c3006c003e2342d2c
candle-lee <bri.candle.li@gmail.com>
2023-09-11 23:30:33 +0900

    styli: add arrow_down component and signinform component

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,10 +1,17 @@
+import Arrow_Down from "../assets/icons/Arrow_Down";
 import SignInForm from "../components/signin/SignInForm";
 
 const SignInPage = () => {
     return (
         <div className="flex justify-between">
-            <div className="mt-[271px] ml-[308px]">
-                <SignInForm />
+            <div className="ml-[308px]">
+                <div className="flex mt-[40px] items-center">
+                    <Arrow_Down />
+                    <a href="" className="text-[#A3AED0] text-sm font-medium leading-[30px] tracking-[-0.28px] ml-[6px]">Back to dashboard</a>
+                </div>
+                <div className="mt-[201px]">
+                    <SignInForm />
+                </div>
             </div>
         </div>
     )
