071dda1e4713c13a7b9ea1bc030d8b8f0bccd170
candle-lee <bri.candle.li@gmail.com>
2023-09-14 16:36:16 +0900

    fix: replace a to Link

--- src/components/common/BackToDashboard.tsx
+++ src/components/common/BackToDashboard.tsx
@@ -1,12 +1,11 @@
-import { useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
 import Arrow_Down from "../../assets/icons/Arrow_Down";
 
 const BackToDashboard = () => {
-    const navigate = useNavigate();
     return (
         <>
             <Arrow_Down />
-            <a href="" onClick={() => {navigate('/')}} className="text-[#A3AED0] text-sm font-medium leading-[30px] tracking-[-0.28px] ml-[6px]">Back to dashboard</a>
+            <Link to='/' className="text-[#A3AED0] text-sm font-medium leading-[30px] tracking-[-0.28px] ml-[6px]">Back to dashboard</Link>
         </>
     )
 }
