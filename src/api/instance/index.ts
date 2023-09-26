3c84663a4d0a87faa591b53c1f4db14c41472604
candle-lee <bri.candle.li@gmail.com>
2023-09-26 10:44:08 +0300

    feat: define axiso instance

---
+++ src/api/instance/index.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { apiUrl } from "../../constant/constants";
+
+export const customHeader = {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+};
+
+export const useAxios = () => {
+    const instance = axios.create({
+        baseURL: apiUrl,
+        headers: customHeader,
+    });
+    
+    instance.interceptors.request.use((requestConfig) => {
+        const updatedConfig = {...requestConfig}
+        const token = localStorage.getItem('token');
+        if (token) {
+            updatedConfig.headers.Authorization = `Bearer ${token}`;
+        } else if (updatedConfig.method === "get") {
+            // Stop the GET request when no authorization header is present
+            return Promise.reject(new Error("Authorization header is missing"));
+          }
+        return updatedConfig;
+    }, (error) => {
+        return Promise.reject(error);
+    });
+    return instance;
+}
