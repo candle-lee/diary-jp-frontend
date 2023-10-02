d3b6713df73fc9c0636bc869cda7165b7727cc1f
candle-lee <bri.candle.li@gmail.com>
2023-10-02 11:44:36 +0300

    feat: define home, signin and signup pages

--- src/pages/signin.page.tsx
+++ src/pages/signin.page.tsx
@@ -4,11 +4,11 @@ import SignInForm from "../components/signin/SignInForm";
 const SignInPage = () => {
     return (
         <div className="flex justify-between">
-            <div className="ml-[308px]">
-                <div className="flex mt-[40px] items-center">
+            <div className="ml-[308px] mt-[40px]">
+                <div className="flex items-center">
                    <BackToDashboard />
                 </div>
-                <div className="mt-[201px]">
+                <div className="mt-[100px]">
                     <SignInForm />
                 </div>
             </div>
