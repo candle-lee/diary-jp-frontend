a7da1dad2a22681e7d49614da99203a819e9bded
candle-lee <bri.candle.li@gmail.com>
2023-10-05 05:50:09 +0300

    config: define main route

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import { CircleSpinner } from './components/common/CircleSpinner';
 import ResetPasswordPage from './pages/resetpassword.page';
+import MainPage from './pages/main.page';
 
 const HomePage = lazy(() => import('./pages/home.page'));
 const SignInPage = lazy(() => import('./pages/signin.page'));
@@ -20,6 +21,7 @@ function App() {
           <ToastContainer />
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path='/main' element={<MainPage />} />
             <Route path="/signin" element={<SignInPage />} />
             <Route path="/signup" element={<SignUpPage />} />
             <Route path="/forgetpassword" element={<ForgetPasswordPage />} />
