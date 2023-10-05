22dda552ab4f52743d5b8b96d895a26572dad269
candle-lee <bri.candle.li@gmail.com>
2023-10-05 07:43:04 +0300

    feat: complete AvatarCard component

--- src/components/common/DefaultAvatar.tsx
+++ src/components/common/DefaultAvatar.tsx
@@ -1,18 +1,26 @@
 'use client';
 
-import { Avatar } from 'flowbite-react';
+import { Avatar, Label } from 'flowbite-react';
 
 const DefaultAvatar = () => {
   return (
-    <div className='w-87 rounded-full ring ring-white ring-offset-base-100 ring-offset-2'>
-      <Avatar
-        size={'87px'}
-        color={'light'}
-        alt="avatar of Jese"
-        img="/src/assets/avatar/avatar1.png"
-        rounded
-      />
-    </div>
+    <>
+      <div className='w-[87px] rounded-full ring ring-white ring-offset-base-100 ring-offset-2 mx-auto'>
+        <Avatar
+          size={'87px'}
+          color={'light'}
+          alt="avatar of Jese"
+          img="/src/assets/avatar/avatar1.png"
+          rounded
+        />
+      </div>
+      <div className="text-center text-[#2B3674] font-bold font-dm-sans text-xl leading-[32px] tracking-[-0.4px] mt-[15px]">
+        Adela Parkson
+      </div>
+      <div className='text-center text-[#A3AED0] font-bold text-[14px] leading-6 tracking-[-0.28px] '>
+        How was your day today?
+      </div>
+    </>
   )
 }
 
--- src/components/main/AvatarCard.tsx
+++ src/components/main/AvatarCard.tsx
@@ -1,12 +1,18 @@
 import DefaultAvatar from "../common/DefaultAvatar";
+import QuantityBox from "./QuantityBox";
 
 const AvatarCard = () => {
     return (
-        <div className="w-[552px] h-[365px] flex-shrink-0 rounded-[20px] bg-[#FFF]">
-            <div className="bg-[url('/src/assets/img/avatar-background.png')] bg-cover bg-gray-50 w-[518px] h-[131px] mx-[17px] mt-[18px] flex-shrink-0 rounded-2xl bg-center  bg-no-repeat flex justify-center">
-                <div className="-mb-[40px] flex items-end">
+        <div className="w-[552px] h-[365px] flex-shrink-0 rounded-[20px] bg-[#FFF] px-[17px] pt-[18px]">
+            <div className="bg-[url('/src/assets/img/avatar-background.png')] bg-cover bg-gray-50 w-[518px] h-[131px] flex-shrink-0 rounded-2xl bg-center  bg-no-repeat flex justify-center">
+                <div className="absolute mt-[85px]">
                     <DefaultAvatar />
                 </div>
+            </div>
+            <div className="flex justify-center mt-[133px] gap-4">
+                <QuantityBox />
+                <QuantityBox />
+                <QuantityBox />
             </div>
         </div>
     )
