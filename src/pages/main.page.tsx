7a5659e95736502960a8e856c2b7188048393a7d
candle-lee <bri.candle.li@gmail.com>
2023-10-12 10:45:56 +0300

    feat: define storagecard component

--- src/pages/main.page.tsx
+++ src/pages/main.page.tsx
@@ -1,5 +1,6 @@
 import AvatarCard from "../components/main/AvatarCard";
 import SearchBar from "../components/main/SearchBar";
+import StorageCard from "../components/main/StorageCard";
 
 const MainPage = () => {
   return (
@@ -7,8 +8,17 @@ const MainPage = () => {
       <div className="flex justify-end">
         <SearchBar />
       </div>
-      <div className="flex">
-        <AvatarCard />
+      <div className="grid grid-cols-12 gap-5">
+        <div className="col-span-12 sm:col-span-4 md:col-span-3 lg:col-span-4 xl:col-span-5">
+          <AvatarCard />
+        </div>
+        <div className="col-span-12 sm:col-span-4 md:col-span-6 lg:col-span-5 xl:col-span-3">
+          <StorageCard />
+        </div>
+        <div className="col-span-12 sm:col-span-4 md:col-span-3 lg:col-span-3 xl:col-span-4">
+          <AvatarCard />
+        </div>
+        {/* <StorageCard /> */}
       </div>
     </div>
   );
