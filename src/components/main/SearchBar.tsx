84fcb7506b0eab1d7818792e9a1f2b6219be7305
candle-lee <bri.candle.li@gmail.com>
2023-10-13 04:55:38 +0300

    style: fix SearchBar styles

--- src/components/main/SearchBar.tsx
+++ src/components/main/SearchBar.tsx
@@ -6,16 +6,16 @@ import SearchInput from "./SearchInput";
 
 const SearchBar = () => {
   return (
-    <div className="flex p-[10px] w-[422px] h-[61px] rounded-[30px] bg-[#FFF] shadow-[14px 17px 40px 4px] shadow-[rgba(112, 144, 176, 0.08)] p-[10px]">
-      <div className="flex justify-start">
+    <div className="flex justify-around p-[10px] w-[422px] h-[61px] rounded-[30px] bg-[#FFF] shadow-[14px 17px 40px 4px] shadow-[rgba(112, 144, 176, 0.08)] p-[10px]">
+      <div className="">
         <SearchInput />
       </div>
-      <div className="flex items-center">
+      <div className="flex items-center xl:gap-2 lg:gap-1 md:gap-1 sm:gap-0">
         <RingSVGIcon />
         <MoonSVGIcon />
         <InfoSVGIcon />
       </div>
-      <div className="flex justify-end">
+      <div className="ml-[5px]">
         <DefaultAvatar size="md" />
       </div>
     </div>
