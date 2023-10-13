ffc8653b32906b2f2a71baa60a2f1e9831dd17de
candle-lee <bri.candle.li@gmail.com>
2023-10-13 05:15:34 +0300

    style: fix margin top of button

--- src/components/main/VideoUploadCard.tsx
+++ src/components/main/VideoUploadCard.tsx
@@ -18,7 +18,7 @@ const VideoUploadCard = () => {
         </ul>
         <button
           type="button"
-          className="w-full mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          className="w-full mt-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
         >
           Record Now
         </button>
