df43d107f3427f46838281b55258c2f6fc1006a4
candle-lee <bri.candle.li@gmail.com>
2023-10-16 18:17:52 +0900

    clean codebase

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 import "flowbite";
 import { store } from "./redux/store.ts";
 import { Provider } from "react-redux";
-// import './assets/tailwindcss.css'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
