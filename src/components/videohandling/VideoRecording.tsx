01ddeabe13fea12f18400ead5583876acaab0ee9
candle-lee <bri.candle.li@gmail.com>
2023-11-12 23:03:00 -0600

    complete Video component

--- src/components/videohandling/VideoRecording.tsx
+++ src/components/videohandling/VideoRecording.tsx
@@ -1,17 +1,39 @@
-import React, { useCallback, useState } from "react";
-import VideoRecorder from "react-video-recorder";
+import { Button } from "flowbite-react";
+import React, { useCallback, useEffect, useState } from "react";
+import {
+  useRecordWebcam,
+} from "react-record-webcam";
 
 const VideoRecording: React.FC = () => {
-  const [recording, setRecording] = useState(false);
+  const [recorder, setRecorder] = useState(undefined);
+  const [starting, setStartind] = useState(false);
   const [pausing, setPausing] = useState(false);
   const [stoping, setStoping] = useState(false);
+  const {
+    activeRecordings,
+    createRecording,
+    openCamera,
+    startRecording,
+    stopRecording,
+    download,
+  } = useRecordWebcam();
+  let recording : any;
+  const initialRecording = async () => {
+    recording = await createRecording();
+    if (!recording) return;
+    setRecorder(recording);
+    await openCamera(recording.id);
+  }
+  useEffect(() => {
+    initialRecording();
+  }, []);
 
   const handlePauseResumeClick = useCallback(() => {
     setPausing((prevPausing) => !prevPausing);
   }, []);
 
   const handleStopClick = useCallback(() => {
-    setRecording(false);
+    setStartind(false);
     setStoping(true);
   }, []);
 
@@ -22,64 +44,73 @@ const VideoRecording: React.FC = () => {
   const handleNewClick = useCallback(() => {
     console.log("New clicked");
     setStoping(false);
-    setRecording(false);
+    setStartind(false);
   }, []);
 
   const handleStartCaptureClick = useCallback(() => {
-    setRecording(true);
+    setStartind(true);
   }, []);
-
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <div className="w-full px-20" style={{ maxHeight: "90VH" }}></div>
-      <VideoRecorder
-        onRecordingComplete={(videoBlob) => {
-          // Do something with the video...
-          console.log("videoBlob", videoBlob);
-        }}
-      />
+      <div className="w-full px-10">
+        {activeRecordings.map(recording => (
+          <div key={recording.id}>
+            <video className="w-full"  style={{ maxHeight: "90VH" }} ref={recording.webcamRef} autoPlay muted />
+          </div>
+        ))}
+      </div>
       <div className="mt-4">
-        {!recording && !stoping && (
-          <button
-            onClick={handleStartCaptureClick}
-            className="bg-green-500 text-white py-2 px-4 rounded"
+        {!starting && !stoping && (
+          <Button
+            onClick={async ()=>{
+              handleStartCaptureClick();
+              await startRecording(recorder.id);
+              await new Promise((resolve) => setTimeout(resolve, 3000));
+            }}
+            className="w-full bg-green-500 text-white py-2 px-4 rounded"
           >
             Start
-          </button>
+          </Button>
         )}
-        {recording && (
-          <>
-            <button
+        {starting && (
+          <div className="flex">
+            <Button
               onClick={handlePauseResumeClick}
               className="bg-yellow-500 text-white py-2 px-4 rounded"
             >
               {pausing ? "Resume" : "Pause"}
-            </button>
-            <button
-              onClick={handleStopClick}
+            </Button>
+            <Button
+              onClick={async () => {
+                handleStopClick();
+                await stopRecording(recorder.id);
+              }}
               className="ml-2 bg-red-500 text-white py-2 px-4 rounded"
             >
               Stop
-            </button>
-          </>
+            </Button>
+          </div>
+        )}
+        {stoping && (
+          <div className="flex">
+            <Button
+              onClick={async () => {
+                handleSaveClick();
+                await download(recorder.id);
+              }}
+              className="bg-blue-500 text-white py-2 px-4 rounded"
+            >
+              Save
+            </Button>
+            <Button
+              onClick={handleNewClick}
+              className="ml-2 bg-indigo-500 text-white py-2 px-4 rounded"
+            >
+              New
+            </Button>
+          </div>
         )}
       </div>
-      {stoping && (
-        <div className="mt-4">
-          <button
-            onClick={handleSaveClick}
-            className="bg-blue-500 text-white py-2 px-4 rounded"
-          >
-            Save
-          </button>
-          <button
-            onClick={handleNewClick}
-            className="ml-2 bg-indigo-500 text-white py-2 px-4 rounded"
-          >
-            New
-          </button>
-        </div>
-      )}
     </div>
   );
 };
