9e4401af509d575565b1f37c542cb7d437987a22
candle-lee <bri.candle.li@gmail.com>
2023-11-20 03:24:39 +0100

    perf: refactor ForgetPassword page

---
+++ src/components/pages/forgetpassword/useForgetPasswordForm.ts
@@ -0,0 +1,33 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useResetPassPasscode } from "../../../api/auth/hooks/useResetPassPasscode";
+
+export const useForgetPasswordForm = () => {
+  const { mutate, isLoading, error } = useResetPassPasscode();
+
+  const validationSchema = z.object({
+    email: z.string().min(1, "Email is required").email("Must be a valid email"),
+    isAccepted: z.literal(true, {
+        errorMap: () => ({ message: "You must accept Terms and Conditions" }),
+      })
+  });
+
+  type ValidationSchema = z.infer<typeof validationSchema>;
+
+  const { register, handleSubmit, formState: { errors } } = useForm<ValidationSchema>({
+    defaultValues: {
+      email: "",
+      isAccepted: undefined,
+    },
+    resolver: zodResolver(validationSchema),
+  });
+
+  const onSubmit: SubmitHandler<ValidationSchema> = async (
+    data: ValidationSchema
+  ) => {
+    mutate(data.email);
+  };
+
+  return { register, handleSubmit, onSubmit, errors, isLoading, serverError: error };
+};
