4e1e8fc89d24a33d05cf8f0436911732f5f257a7
candle-lee <bri.candle.li@gmail.com>
2023-12-29 05:15:12 +0100

    style: update styling in BackToDashboard

--- src/components/common/BackToDashboard.tsx
+++ src/components/common/BackToDashboard.tsx
@@ -5,7 +5,7 @@ const BackToDashboard: React.FC = () => {
   return (
     <Link to="/" className="flex items-center">
       <ArrowDownSVGIcon />
-      <p className="text-[#A3AED0] text-sm font-medium leading-[30px] tracking-[-0.28px] ml-[6px]">
+      <p className="text-[#A3AED0] text-sm font-medium leading-[30px] tracking-[-0.28px]">
         Back to dashboard
       </p>
     </Link>
