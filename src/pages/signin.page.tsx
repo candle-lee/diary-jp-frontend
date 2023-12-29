1ff678d70c3e7c959b90f6ac65bdb0b775bff999
candle-lee <bri.candle.li@gmail.com>
2023-12-29 09:13:00 +0100

    perf: complete the signin page

--- src/pages/signin.page.tsx
+++ src/pages/signin.page.tsx
@@ -1,12 +1,14 @@
+import { BackToDashboard } from "../components/common";
 import { SignInForm } from "../components/pages";
-import { ImageSection } from "../components/common";
 
 const SignInPage: React.FC = () => {
   return (
-    <div className="bg-white dark:bg-gray-900">
-      <div className="grid lg:h-screen lg:grid-cols-2">
+    <div className="bg-[#FFF] dark:bg-gray-900 flex items-center justify-center min-h-screen">
+      <div className="w-full max-w-md px-4">
+        <div className="mb-6">
+          <BackToDashboard />
+        </div>
         <SignInForm />
-        <ImageSection />
       </div>
     </div>
   );
