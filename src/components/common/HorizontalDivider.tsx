5c16fb12dedb3576da19d707ed202d7ede35b2fb
candle-lee <bri.candle.li@gmail.com>
2023-12-29 03:42:45 +0100

    style: update styling based on design

--- src/components/common/HorizontalDivider.tsx
+++ src/components/common/HorizontalDivider.tsx
@@ -1,11 +1,11 @@
 const HorizontalDivider: React.FC = () => {
   return (
     <div className="flex items-center">
-      <div className="h-0.5 w-full bg-gray-200 dark:bg-gray-700"></div>
-      <div className="px-5 text-center text-gray-500 dark:text-gray-400">
+      <div className="h-0.5 w-full bg-[#E0E5F2] dark:bg-gray-700"></div>
+      <span className="px-4 text-center text-[#A3AED0] text-sm font-medium leading-6 tracking-[-0.0175rem] dark:text-gray-400">
         or
-      </div>
-      <div className="h-0.5 w-full bg-gray-200 dark:bg-gray-700"></div>
+      </span>
+      <div className="h-0.5 w-full bg-[#E0E5F2] dark:bg-gray-700"></div>
     </div>
   );
 };
