7414e56db8aabd83883d4300b835cf18360a3c45
candle-lee <bri.candle.li@gmail.com>
2024-01-18 07:42:15 +0100

    feat: add icons

---
+++ src/components/icons/VideoPlayIcon.tsx
@@ -0,0 +1,18 @@
+const VideoPlayIcon: React.FC = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="17"
+      height="18"
+      viewBox="0 0 17 18"
+      fill="none"
+    >
+      <path
+        d="M15.5 7.26795C16.8333 8.03775 16.8333 9.96225 15.5 10.7321L3.5 17.6603C2.16667 18.4301 0.499999 17.4678 0.499999 15.9282L0.5 2.0718C0.5 0.532196 2.16667 -0.430055 3.5 0.339746L15.5 7.26795Z"
+        fill="#D9D9D9"
+      />
+    </svg>
+  );
+};
+
+export default VideoPlayIcon;
