f3fd1d911508733d477fd319cff48e9071ece9fe
candle-lee <bri.candle.li@gmail.com>
2024-01-18 08:21:16 +0100

    fix: update styling for full screen

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -4,7 +4,7 @@ import { CircleSpinner } from "../components/common";
 
 const Layout = () => {
   return (
-    <div className="flex flex-col w-full">
+    <div className="flex flex-col w-full h-screen">
       <Suspense
         fallback={
           <div className="flex h-full items-center justify-center">
@@ -12,7 +12,7 @@ const Layout = () => {
           </div>
         }
       >
-        <div className="flex flex-col h-full">
+        <div className="flex flex-col flex-grow">
           <div className="sticky top-0 flex items-center bg-[#000] lg:h-16 h-8 border-b border-white border-opacity-15">
             <p className="text-[#FFF] font-sans lg:text-2xl md:text-base sm:text-base font-normal lg:leading-6 md:leading-4 sm:leading-4 p-2">
               U-DATA
