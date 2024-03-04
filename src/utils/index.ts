f40b62f6595141d47f736f47c25ed3835df92d4f
candle-lee <bri.candle.li@gmail.com>
2024-03-04 06:15:22 +0100

    feat: add convertByte

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,16 @@
 export const converToDateTime = (dateTime: string | undefined) => {
-    return dateTime ? new Date(parseInt(dateTime, 10)).toUTCString() : null;
-}
+  return dateTime ? new Date(parseInt(dateTime, 10)).toUTCString() : null;
+};
+
+export const convertBytes = (bytes: number) => {
+  const units = ["bytes", "KB", "MB", "GB"];
+  let unitIndex = 0;
+  let convertedValue = bytes;
+
+  while (convertedValue >= 1024 && unitIndex < units.length - 1) {
+    convertedValue /= 1024;
+    unitIndex++;
+  }
+
+  return convertedValue.toFixed(2) + " " + units[unitIndex];
+};
