a2b27e5b0cc8d6c51c8a9271842b2d3df6b1a818
candle-lee <bri.candle.li@gmail.com>
2024-03-15 02:56:48 +0200

    fix: background of loading page

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -8,12 +8,12 @@ const Layout: React.FC = () => {
     <div className="flex flex-col w-full h-screen">
       <Suspense
         fallback={
-          <div className="flex h-full items-center justify-center">
+          <div className="flex h-full items-center justify-center bg-black">
             <CircleSpinner />
           </div>
         }
       >
-        <div className="flex flex-col flex-grow">
+        <div className="flex flex-col flex-grow bg-black">
           <Header />
           <Outlet />
         </div>
