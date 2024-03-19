db6b55950e3fdc76c1e1073b51ba654204c70263
candle-lee <bri.candle.li@gmail.com>
2024-03-19 07:53:55 +0200

    feat: define fetchGoogleUserInfo

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios from "axios";
+
 export const converToDateTime = (dateTime: string | undefined) => {
   return dateTime ? new Date(parseInt(dateTime, 10)).toUTCString() : null;
 };
@@ -21,4 +24,21 @@ export const getCurrentTime = () => {
   const minutes = String(now.getMinutes()).padStart(2, "0");
   const seconds = String(now.getSeconds()).padStart(2, "0");
   return `${hours}:${minutes}:${seconds}`;
+};
+
+export const fetchGoogleUserInfo = async (
+  accessToken: string
+): Promise<any> => {
+  const userInfoUrl = "https://www.googleapis.com/oauth2/v2/userinfo";
+
+  try {
+    const response = await axios.get(userInfoUrl, {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching Google user info:", error);
+    throw error;
+  }
 };
