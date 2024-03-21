4f9f2e9f955a9fbb322332561c2e6d5bdff4cfa7
candle-lee <bri.candle.li@gmail.com>
2024-03-21 13:31:18 +0200

    fix: close webcam

--- src/components/pages/videohandling/VideoRecording.tsx
+++ src/components/pages/videohandling/VideoRecording.tsx
@@ -35,11 +35,11 @@ const VideoRecording: React.FC = () => {
 
   const getRecordingFileHooks = async () => {
     try {
-      closeCamera(activeRecordings[0].id);
       const recorded = await stopRecording(activeRecordings[0].id);
       const formData = new FormData();
       formData.append('file', recorded?.blobChunks[0] as Blob, Date.now().toString());
       mutate(formData);
+      closeCamera(activeRecordings[0].id);
     } catch (error) {
       console.error("Error getting recording:", error);
     }
@@ -64,61 +64,61 @@ const VideoRecording: React.FC = () => {
 
   return (
     <>
-      {activeRecordings.map((recording) => (
-        <div className="flex flex-col h-full" key={recording.id}>
-          <div className="relative h-full">
-            <video className="aspect-video object-cover h-full mx-auto" ref={recording.webcamRef} autoPlay muted />
-            {isStarted && (
-              <div className="absolute inset-x-0 top-4 flex justify-center">
-                <RecordingCounter isStopped={isStarted && !isRecording} />
-              </div>
-            )}
-            {!isRecording &&
-              (
-                <div
-                  className="absolute right-[0.63rem] top-[0.63rem] cursor-pointer lg:right-4 lg:top-4"
-                  onClick={toggleDialog}
-                >
-                  <CrossIcon />
+        {activeRecordings.map((recording) => (
+      <div className="flex flex-col h-full" key={recording.id}>
+            <div className="relative h-full">
+              <video className="aspect-video object-cover h-full mx-auto" ref={recording.webcamRef} autoPlay muted />
+              {isStarted && (
+                <div className="absolute inset-x-0 top-4 flex justify-center">
+                  <RecordingCounter isStopped={isStarted && !isRecording} />
                 </div>
-              )
-            }
-          </div>
+              )}
+              {!isRecording &&
+                (
+                  <div
+                    className="absolute right-[0.63rem] top-[0.63rem] cursor-pointer lg:right-4 lg:top-4"
+                    onClick={toggleDialog}
+                  >
+                    <CrossIcon />
+                  </div>
+                )
+              }
+            </div>
           {/* {captionShow && (
             <div className="relative lg:left-[56rem] lg:w-[20rem]">
               <CaptionContainer />
               <CaptionContainer />
             </div>
           )} */}
-          <div className="mt-auto flex justify-between items-center">
-            <div className="flex justify-center items-center py-6 gap-6 relative" style={{ flex: 2 }}>
-              {isRecording ? (
-                <button className="z-50" onClick={() => handleRecordingPause()}>
-                  <InRecordingIcon />
-                </button>
-              ) : (
-                <button className="z-50" onClick={() => handleRecordingStart()}>
-                  <StartRecordingIcon />
-                </button>
-              )}
-              {/* <div className="cursor-pointer absolute right-28 lg:right-[32rem]" onClick={() => setCaptionShow(!captionShow)}>
+            <div className="mt-auto flex justify-between items-center">
+              <div className="flex justify-center items-end py-6" style={{ flex: 2 }}>
+                {isRecording ? (
+                  <button className="z-50" onClick={() => handleRecordingPause()}>
+                    <InRecordingIcon />
+                  </button>
+                ) : (
+                  <button className="z-50" onClick={() => handleRecordingStart()}>
+                    <StartRecordingIcon />
+                  </button>
+                )}
+                {/* <div className="cursor-pointer absolute right-28 lg:right-[32rem]" onClick={() => setCaptionShow(!captionShow)}>
                 <CaptionIcon />
               </div> */}
-            </div>
-            {isStarted && !isRecording && (
-              <div className="absolute right-[1.31rem]">
-                <button
-                  type="button"
-                  onClick={() => getRecordingFileHooks()}
-                  className="bg-[#FFF] rounded-2xl py-1 px-3 text-black text-sm font-normal leading-[125%] tracking-[-0.0175rem]"
-                >
-                  Upload
-                </button>
               </div>
-            )}
-          </div>
-        </div>
-      ))}
+              {isStarted && !isRecording && (
+                <div className="absolute right-[1.31rem]">
+                  <button
+                    type="button"
+                    onClick={() => getRecordingFileHooks()}
+                    className="bg-[#FFF] rounded-2xl py-1 px-3 text-black text-sm font-normal leading-[125%] tracking-[-0.0175rem]"
+                  >
+                    Upload
+                  </button>
+                </div>
+              )}
+            </div>
+      </div>
+        ))}
       {isDialogOpen && (
         <div className="absolute inset-0 flex items-center justify-center z-10">
           <div className="bg-black bg-opacity-80 border border-solid border-white border-opacity-25 rounded-lg p-4 flex flex-col gap-4 max-w-60">
