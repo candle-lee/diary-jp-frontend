934f6c634cca4c45e8f767e58c5685ca55882f7d
candle-lee <bri.candle.li@gmail.com>
2024-03-25 14:50:48 +0900

    fix: caption container

--- src/components/pages/videohandling/CaptionContainer.tsx
+++ src/components/pages/videohandling/CaptionContainer.tsx
@@ -1,14 +1,18 @@
 import React from "react"
-import { getCurrentTime } from "../../../utils";
 
-const CaptionContainer: React.FC = () => {
+interface CaptionContainerProps {
+    index: any,
+    transcript: any,
+}
+
+const CaptionContainer: React.FC<CaptionContainerProps> = ({ index, transcript }) => {
     return (
-        <div className="p-2 flex flex-col gap-1 bg-white bg-opacity-30 rounded-lg m-[10px]">
+        <div className={`p-2 flex flex-col gap-1 bg-white bg-opacity-80 rounded-lg m-[10px] ${index == 3 ? "opacity-60 lg:opacity-100" : index == 4 ? "opacity-60 hidden lg:block" : index == 5 ? "opacity-40 hidden lg:block" : index == 6 ? "opacity-20 hidden lg:block" : "opacity-100"}`}>
             <div className="text-black text-xs font-normal leading-[124%]">
-                {getCurrentTime()}
+                {transcript.time.format("hh:mm:ss")}
             </div>
             <div className="text-black text-sm font-normal leading-[125%] tracking-[-0.0175rem]">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                {transcript.caption}
             </div>
         </div>
     )
