7441f04018f2ebdf4a25996aedc517e15f7a121c
candle-lee <bri.candle.li@gmail.com>
2024-03-25 14:52:56 +0900

    fix: app init socket

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import { useEffect } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
 import { PrivateRoutes, PublicRoutes } from "./router";
+import initSocket from "./app/initSocket";
 
 function App() {
   const client = new QueryClient();
   const router = createBrowserRouter([PrivateRoutes(), PublicRoutes()]);
   
+  useEffect(() => {
+    initSocket();
+  }, []);
+
   return (
     <>
       <QueryClientProvider client={client}>
