c0ae203fe9d0c0122cec2daf0c676d3f489e7f66
candle-lee <bri.candle.li@gmail.com>
2024-03-27 03:39:36 +0200

    style: update InputField and PasswordInputField

--- src/components/common/InputField.tsx
+++ src/components/common/InputField.tsx
@@ -9,22 +9,22 @@ const InputField: React.FC<IInputItem> = ({
   error,
 }: IInputItem) => {
   return (
-    <div className="flex flex-col gap-[0.44rem]">
+    <div className="flex flex-col gap-2">
       <label
         htmlFor={inputName}
-        className="block text-white font-sans text-xs font-normal leading-3"
+        className="block text-white font-sans text-xs font-normal leading-[100%]"
       >
         {description} <span className="text-[#ED2B2B]">*</span>
       </label>
       <input
         type={inputType}
         id={inputName}
-        className="bg-black text-white rounded-xl border border-solid border-white border-opacity-15 w-full p-4 placeholder:font-sans placeholder:text-sm placeholder:font-normal placeholder:leading-[1.09375rem] placeholder:text-white placeholder:text-opacity-60 placeholder:tracking-[-0.0175rem]"
+        className="bg-black text-white rounded-xl border border-solid border-white w-full p-4 placeholder:font-sans placeholder:text-sm placeholder:font-normal placeholder:leading-[125%] placeholder:text-white placeholder:text-opacity-60 placeholder:tracking-[-0.0175rem]"
         placeholder={placeholderText}
         {...register(inputName)}
       />
       <p
-        className="text-start text-xs font-normal leading-[124%] text-[#ED2B2B] mt-2"
+        className="text-start text-xs font-normal leading-[124%] text-[#ED2B2B]"
         style={{ display: error ? "block" : "none" }}
       >
         {error}
--- src/components/common/PasswordInputField.tsx
+++ src/components/common/PasswordInputField.tsx
@@ -27,7 +27,7 @@ const PasswordInputField: React.FC<IInputItem> = ({
         <input
           type={isPasswordVisible ? "text" : "password"}
           id={inputName}
-          className="bg-black text-white rounded-xl border border-solid border-white border-opacity-15 w-full p-4 placeholder:font-sans placeholder:text-sm placeholder:font-normal placeholder:leading-[1.09375rem] placeholder:text-white placeholder:text-opacity-60 placeholder:tracking-[-0.0175rem]"
+          className="bg-black text-white rounded-xl border border-solid border-white w-full p-4 placeholder:font-sans placeholder:text-sm placeholder:font-normal placeholder:leading-[125%] placeholder:text-white placeholder:text-opacity-60 placeholder:tracking-[-0.0175rem]"
           placeholder={placeholderText}
           {...register(inputName)}
         />
@@ -38,7 +38,7 @@ const PasswordInputField: React.FC<IInputItem> = ({
         />
       </div>
       <p
-        className="text-start text-xs font-normal leading-[124%] text-[#ED2B2B] mt-2"
+        className="text-start text-xs font-normal leading-[124%] text-[#ED2B2B]"
         style={{ display: error ? "block" : "none" }}
       >
         {error}
